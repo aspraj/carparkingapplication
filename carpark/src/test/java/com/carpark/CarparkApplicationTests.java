65c103ef76f23a662cb2b2d041160b5e018c91f7
aspraj3 <amansinghpatel429@gmail.com>
2022-07-23 13:07:15 +0530

    new commit

--- carpark/src/test/java/com/carpark/CarparkApplicationTests.java
+++ carpark/src/test/java/com/carpark/CarparkApplicationTests.java
@@ -1,13 +1,13 @@
-package com.carpark;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CarparkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.carpark;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CarparkApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
